a08e840d33e690836214dca276457105dce10670
fmagneAsus <fmagne32@hotmail.com>
2019-08-13 19:20:44 -0400

    Actividad Login

--- app/src/main/java/com/fmagne/ardayasrl/MainActivity.java
+++ app/src/main/java/com/fmagne/ardayasrl/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 Intent myIntent = new Intent(MainActivity.this, ActividadUno.class);
                 startActivity(myIntent);
                 //finish();
